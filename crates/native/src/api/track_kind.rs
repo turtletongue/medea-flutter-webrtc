1506b63c55c1df55bedaeab8d766cb61688b4de5
turtletongue <platonsterh@gmail.com>
2025-07-02 22:48:01 +0500

    Unignore TrackKind to move it in separate module

--- crates/native/src/api/track_kind.rs
+++ crates/native/src/api/track_kind.rs
@@ -2,12 +2,14 @@
 //!
 //! [1]: https://w3.org/TR/mediacapture-streams#dfn-kind
 
+use flutter_rust_bridge::frb;
 use libwebrtc_sys as sys;
 
 /// [MediaStreamTrack.kind][1] representation.
 ///
 /// [1]: https://w3.org/TR/mediacapture-streams#dfn-kind
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
+#[frb(unignore)]
 pub enum TrackKind {
     /// Audio track.
     Audio,
